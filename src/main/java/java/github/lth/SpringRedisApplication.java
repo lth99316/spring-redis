fe80c7e1808d33a2c2f74f3468d0cb34bd865191
lethanhhoa <lethanhhoaxyz@outlook.com>
2025-02-20 15:39:53 +0700

    init

---
+++ src/main/java/java/github/lth/SpringRedisApplication.java
@@ -0,0 +1,13 @@
+package java.github.lth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRedisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringRedisApplication.class);
+
+    }
+}
